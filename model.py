b2ee5aba0799db6f7e1fc3c9fcf9b189aa9e8561
Ankit Negi <89020930+serAnkii@users.noreply.github.com>
2021-12-24 14:51:40 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,35 +1,54 @@
 # Importing the libraries
 import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
 import matplotlib.pyplot as plt
 import pandas as pd
 import pickle
 
-dataset = pd.read_csv('hiring.csv')
+dataset = pd.read_csv('credit_data.csv')
+credit_card_data.head()
+credit_card_data.tail()
+credit_card_data.info()
+credit_card_data.isnull().sum()
+# distribution of legit transactions & fraudulent transactions
+credit_card_data['Class'].value_counts()
+# separating the data for analysis
+legit = credit_card_data[credit_card_data.Class == 0]
+fraud = credit_card_data[credit_card_data.Class == 1]
+print(legit.shape)
+print(fraud.shape)
+legit.Amount.describe()
+fraud.Amount.describe()
+# compare the values for both transactions
+credit_card_data.groupby('Class').mean()
+legit_sample = legit.sample(n=492)
+new_dataset = pd.concat([legit_sample, fraud], axis=0)
+new_dataset.head()
+new_dataset.tail()
+new_dataset['Class'].value_counts()
+new_dataset.groupby('Class').mean()
+X = new_dataset.drop(columns='Class', axis=1)
+Y = new_dataset['Class']
+print(y)
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, 
+print(X.shape, X_train.shape, X_test.shape)
 
-dataset['experience'].fillna(0, inplace=True)
+                                                    model = LogisticRegression()
+                                                    # training the Logistic Regression Model with Training Data
+model.fit(X_train, Y_train)
+                                                    
 
-dataset['test_score'].fillna(dataset['test_score'].mean(), inplace=True)
 
-X = dataset.iloc[:, :3]
-
-#Converting words to integer values
-def convert_to_int(word):
-    word_dict = {'one':1, 'two':2, 'three':3, 'four':4, 'five':5, 'six':6, 'seven':7, 'eight':8,
-                'nine':9, 'ten':10, 'eleven':11, 'twelve':12, 'zero':0, 0: 0}
-    return word_dict[word]
-
-X['experience'] = X['experience'].apply(lambda x : convert_to_int(x))
-
-y = dataset.iloc[:, -1]
-
-#Splitting Training and Test Set
-#Since we have a very small dataset, we will train our model with all availabe data.
-
-from sklearn.linear_model import LinearRegression
-regressor = LinearRegression()
-
-#Fitting model with trainig data
-regressor.fit(X, y)
+X_train_prediction = model.predict(X_train)
+training_data_accuracy = accuracy_score(X_train_prediction, Y_train)
+print('Accuracy on Training data : ', training_data_accuracy)
+# accuracy on test data
+X_test_prediction = model.predict(X_test)
+test_data_accuracy = accuracy_score(X_test_prediction, Y_test)
+print('Accuracy score on Test Data : ', test_data_accuracy)
 
 # Saving model to disk
 pickle.dump(regressor, open('model.pkl','wb'))
